6a5312aba8181462a4e1c6b8799caf98d64ab36a
Sniedze <u.sniedze@gmail.com>
2018-02-15 18:41:51 +0100

    I don`t remember what I did

---
+++ script.js
@@ -0,0 +1,5 @@
+const dataLink = "jason.json";
+const main = document.querySelector("main");
+const template = document.querySelector("template").content;
+const nav = document.querySelector("#nav");
+const all = document.querySelector("#nav a");
