b4012eca149c67eb82d6658e6b29c9a523b803c1
Sniedze <u.sniedze@gmail.com>
2018-02-20 17:42:36 +0100

    Adds some style

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const main = document.querySelector("main");
 const template = document.querySelector("template").content;
 const nav = document.querySelector("#navigation");
@@ -11,42 +10,32 @@ function createContainers(areas) {
 
         const button = document.createElement("button");
         section.id = area;
-
-
         button.textContent = area.coreArea;
-
         main.appendChild(section);
-
-
-
         button.addEventListener("click", () => filter(area));
         nav.appendChild(button);
-
     });
     fetch("jason.json").then(result => result.json()).then(data => show(data));
 }
 
 
 function filter(area) {
-
-
     document.querySelectorAll("main section").forEach(section => {
 
         if (section.id == area) {
+            document.querySelector("button").classList.add("clicked");
             section.classList.remove("hidden");
         } else {
             section.classList.add("hidden");
-
         }
     })
 }
-
 
 function show(data) {
 
     data.forEach(element => {
         console.log("It works");
-        const section = document.querySelector("section")
+        const section = document.querySelector("section");
         const clone = template.cloneNode(true);
         clone.querySelector(".name").textContent = element.coreArea;
         clone.querySelector(".content").textContent = element.content;
