4350e43a5eb706a05da66d7acb4eb1698ce39bdb
Sniedze <u.sniedze@gmail.com>
2018-02-22 12:22:22 +0100

    changes content of Semester information in json

--- script-semesters.js
+++ script-semesters.js
@@ -4,14 +4,14 @@ const nav = document.querySelector("#navigation");
 
 fetch("jason-semesters.json").then(result => result.json()).then(data => createContainers(data));
 
-function createContainers(semesters) {
-    semesters.forEach(semester => {
+function createContainers(areas) {
+    areas.forEach(area => {
         let section = document.createElement("section");
 
         const button = document.createElement("button");
 
-        section.id = semester.semester;
-        button.textContent = semester.semester;
+        section.id = area.semester;
+        button.textContent = area.semester;
         if (button.textContent == "semesterone") {
             button.textContent = "1st Semester";
         }
@@ -26,17 +26,26 @@ function createContainers(semesters) {
         }
         main.appendChild(section);
         button.href = "#";
-        button.addEventListener("click", () => filter(semester));
         nav.appendChild(button);
+        let buttons = document.querySelectorAll("button");
+        buttons[0].focus();
+        filter(areas[0]); //////////////////////////<--- SHOW DATA NO 1 BY DEFAULT, ON LOAD/////////////////////////
+        button.addEventListener("click", () => filter(area));
+
+
     });
+
     fetch("jason-semesters.json").then(result => result.json()).then(data => show(data));
+
+
 }
 
 
-function filter(semester) {
+
+function filter(area) {
     document.querySelectorAll("main section").forEach(section => {
 
-        if (section.id == semester.semester) {
+        if (section.id == area.semester) {
             section.classList.remove('hidden');
 
         } else {
@@ -63,16 +72,28 @@ function show(data) {
             element.semester = "4th Semester";
         }
         clone.querySelector(".name").textContent = element.semester;
-        clone.querySelector(".ects-name").textContent = "Study Content";
+
+
+        clone.querySelector(".ects-name").textContent = "Ects";
         clone.querySelector(".exams-name").textContent = "Exams";
 
+
+
         let ul = document.createElement('ul');
-        clone.querySelector(".ects").appendChild(ul);
+        clone.querySelector(".area-name").appendChild(ul);
 
         element.content.forEach(function (name) {
             let li = document.createElement('li');
             ul.appendChild(li);
             li.innerHTML += name;
+        });
+        let uli = document.createElement('ul');
+        clone.querySelector(".ects").appendChild(uli);
+
+        element.ects.forEach(function (name) {
+            let lis = document.createElement('li');
+            uli.appendChild(lis);
+            lis.innerHTML += name;
         });
         let ulist = document.createElement('ul');
         clone.querySelector(".exams").appendChild(ulist);
@@ -82,6 +103,7 @@ function show(data) {
             ulist.appendChild(list);
             list.innerHTML += name;
         });
+
         section.appendChild(clone);
 
     })
